2c386fd4148f95cafd5c748e3a544dd8b7191c1c
Dup <matdup73@gmail.com>
2020-07-22 15:53:36 +0200

    Updates

---
+++ precache-manifest.a3b704f4ef8f58f9523e6438224654fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f311c7f2b5e09057d53b2445df70759",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c661dcd4ee6d57fac24",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1d0863a737e736a56e34",
+    "url": "/static/css/main.d2428503.chunk.css"
+  },
+  {
+    "revision": "8c661dcd4ee6d57fac24",
+    "url": "/static/js/2.2bda9184.chunk.js"
+  },
+  {
+    "revision": "34f7349b29c742c466ed8a9b31c16f1c",
+    "url": "/static/js/2.2bda9184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0863a737e736a56e34",
+    "url": "/static/js/main.b6e4ff78.chunk.js"
+  },
+  {
+    "revision": "3c69145e7ce484ee8d93",
+    "url": "/static/js/runtime-main.55689cee.js"
+  },
+  {
+    "revision": "b79bf4852239767ec0d4983e42f72f12",
+    "url": "/static/media/cv.b79bf485.pdf"
+  }
+]);
